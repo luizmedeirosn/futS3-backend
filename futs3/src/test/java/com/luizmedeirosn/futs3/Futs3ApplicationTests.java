f3371498e5a6a59195ca479ccf74917707bde563
luizmedeirosn <netomedeirosdev@gmail.com>
2023-09-13 21:12:33 -0300

    feat: project struture

---
+++ futs3/src/test/java/com/luizmedeirosn/futs3/Futs3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizmedeirosn.futs3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Futs3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
