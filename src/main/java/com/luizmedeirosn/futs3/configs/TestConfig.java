fd9a3f4c0072eef3404bd9a120a91821e75c1dc5
luizmedeirosn <netomedeirosdev@gmail.com>
2023-09-16 19:44:04 -0300

    feat: seeding parameters and scores for the attackers

--- src/main/java/com/luizmedeirosn/futs3/configs/TestConfig.java
+++ src/main/java/com/luizmedeirosn/futs3/configs/TestConfig.java
@@ -55,7 +55,11 @@ public class TestConfig implements CommandLineRunner {
         GameMode gm8 = new GameMode( "4-1-2-1-2", "Destaca-se devido à sua formação estreita, em que seus 2 atacantes e seu meia-ofensivo estarão contra os zagueiros do time adversário, na maioria dos cenários. Proporcionando a você muitas oportunidades de ultrapassar a defesa oponente." );
         GameMode gm9 = new GameMode( "4-4-2", "Uma clássica e equilibrada formação, pois permite uma grande variação de posicionamento. Aqui, você terá 4 jogadores na defesa, 4 no meio de campo e 2 atacantes. E torna-se um esquema difícil de enfrentar, visto que você pode usar seus atacantes, as alas ou até mesmo seu meia-central para lançar sequencias de ataque." );
 
-        gameModeRepository.saveAll(Arrays.asList(gm1, gm2, gm3 ,gm4, gm5, gm6, gm7, gm8, gm9));
+        gameModeRepository.saveAll (
+            Arrays.asList (
+                gm1, gm2, gm3 ,gm4, gm5, gm6, gm7, gm8, gm9
+            )
+        );
         
         Position pos1 = new Position( "GOLEIRO",            "GOLEIRO: defensor do gol" );
         Position pos2 = new Position( "LATERAL DIREITO",    "LATERAL DIREITO: responsável por avançar e apoiar a defesa" );
@@ -73,7 +77,14 @@ public class TestConfig implements CommandLineRunner {
         Position pos14 = new Position( "ATACANTE",          "ATACANTE: responsável por concluir as jogadas, marcando gols" );
         Position pos15 = new Position( "CENTROAVANTE",      "CENTROAVANTE: jogador que joga mais próximo à área adversária, responsável por concluir as jogadas com gols" );
 
-        positionRepository.saveAll(Arrays.asList(pos1, pos2, pos3 ,pos4, pos5, pos6, pos7, pos8, pos9, pos10, pos11, pos12, pos13, pos14, pos15));
+        positionRepository.saveAll (
+            Arrays.asList (
+                pos1, pos2, pos3 ,pos4, pos5, pos6, pos7, pos8, pos9, pos10, pos11, pos12, pos13, pos14, pos15
+            )
+        );
+
+        gm1.getPositions().add(pos14);
+        gameModeRepository.save(gm1);
 
         Parameter prmt1 = new Parameter(    "DEFESA DE FALTA",                  null );
         Parameter prmt2 = new Parameter(    "GOL NO PRIMEIRO TEMPO",            null );
@@ -98,11 +109,14 @@ public class TestConfig implements CommandLineRunner {
         Parameter prmt21 = new Parameter(   "CONDUÇÃO",                         null );
         Parameter prmt22 = new Parameter(   "CONTROLE DE BOLA",                 null );
         Parameter prmt23 = new Parameter(   "FRIEZA",                           null );
-        Parameter prmt24 = new Parameter(   "FINALIZAÇÃO",                      null );
-        Parameter prmt25 = new Parameter(   "VELOCIDADE",                       null );
+        Parameter prmt24 = new Parameter(   "VELOCIDADE",                       null );
+        Parameter prmt25 = new Parameter(   "FINALIZAÇÃO",                      null );
+        
 
-        parameterRepository.saveAll(
-            Arrays.asList(prmt1, prmt2, prmt3 ,prmt4, prmt5, prmt6, prmt7, prmt8, prmt9, prmt10, prmt11, prmt12, prmt13, prmt14, prmt15, prmt16, prmt17, prmt18, prmt19, prmt20, prmt21, prmt21, prmt22, prmt23, prmt24, prmt25)
+        parameterRepository.saveAll (
+            Arrays.asList (
+                prmt1, prmt2, prmt3 ,prmt4, prmt5, prmt6, prmt7, prmt8, prmt9, prmt10, prmt11, prmt12, prmt13, prmt14, prmt15, prmt16, prmt17, prmt18, prmt19, prmt21, prmt20, prmt21, prmt16, prmt17, prmt18, prmt19, prmt20, prmt21, prmt21, prmt22, prmt23, prmt24, prmt25
+            )
         );
 
         /*--!> OS JOGADORES ESTÃO ESCALADOS COM POSIÇÕES NO GERAL PARA FACILITAR A MOCKAGEM E PUXAR O RANKING, MAS NÃO SÃO DADOS REAIS */ 
@@ -150,30 +164,104 @@ public class TestConfig implements CommandLineRunner {
          Player player32 = new Player("Fabrício Daniel",    pos14 );
 
         playerRepository.saveAll(
-            Arrays.asList(player1, player2, player3 ,player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18, player19, player20, player21, player21, player22, player23, player24, player25, player26, player27, player28, player29, player30, player31, player32)
+            Arrays.asList (
+                player1, player2, player3 ,player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18, player19, player20, player21, player21, player22, player23, player24, player25, player26, player27, player28, player29, player30, player31, player32
+            )
         );
 
-        PositionParameter pp1 = new PositionParameter(  pos11, prmt15, 100 );
-        PositionParameter pp2 = new PositionParameter(  pos12, prmt12, 25 );
-        PositionParameter pp3 = new PositionParameter(  pos12, prmt14, 25 );
-        PositionParameter pp4 = new PositionParameter(  pos12, prmt25, 50 );
-        PositionParameter pp5 = new PositionParameter(  pos13, prmt14, 20 );
-        PositionParameter pp6 = new PositionParameter(  pos13, prmt23, 30 );
-        PositionParameter pp7 = new PositionParameter(  pos13, prmt25, 50 );
-        PositionParameter pp8 = new PositionParameter(  pos15, prmt12, 20 );
-        PositionParameter pp9 = new PositionParameter(  pos15, prmt19, 20 );
-        PositionParameter pp10 = new PositionParameter( pos15, prmt25, 60 );
+        PositionParameter pp1 = new PositionParameter(  pos14, prmt15, 25 );
+        PositionParameter pp2 = new PositionParameter(  pos14, prmt21, 25 );
+        PositionParameter pp3 = new PositionParameter(  pos14, prmt25, 50 );
 
-        positionParameterRepository.saveAll(Arrays.asList(pp1, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10));
+        positionParameterRepository.saveAll( Arrays.asList(pp1, pp2, pp3) );
 
-        PlayerParameter pylp1 = new PlayerParameter( player11, prmt10, 50 );
-        PlayerParameter pylp2 = new PlayerParameter( player11, prmt17, 60 );
-        PlayerParameter pylp3 = new PlayerParameter( player11, prmt15, 77 );
-        PlayerParameter pylp4 = new PlayerParameter( player26, prmt12, 50 );
-        PlayerParameter pylp5 = new PlayerParameter( player26, prmt19, 80 );
-        PlayerParameter pylp6 = new PlayerParameter( player26, prmt25, 32 );
+        /* ESTATÍSTICA DOS ATACANTES */
+        PlayerParameter pylp1 = new PlayerParameter( player23, prmt15, 30 );
+        PlayerParameter pylp2 = new PlayerParameter( player23, prmt21, 30 );
+        PlayerParameter pylp3 = new PlayerParameter( player23, prmt25, 30 );
 
-        playerParameterRepository.saveAll(Arrays.asList(pylp1, pylp2, pylp3, pylp4, pylp5, pylp6));
+        PlayerParameter pylp4 = new PlayerParameter( player24, prmt15, 90 );
+        PlayerParameter pylp5 = new PlayerParameter( player24, prmt21, 90 );
+        PlayerParameter pylp6 = new PlayerParameter( player24, prmt25, 90 );
+
+        PlayerParameter pylp7 = new PlayerParameter( player25, prmt15, 50 );
+        PlayerParameter pylp8 = new PlayerParameter( player25, prmt21, 50 );
+        PlayerParameter pylp9 = new PlayerParameter( player25, prmt25, 50 );
+
+        PlayerParameter pylp10 = new PlayerParameter( player26, prmt15, 100 );
+        PlayerParameter pylp11 = new PlayerParameter( player26, prmt21, 100 );
+        PlayerParameter pylp12 = new PlayerParameter( player26, prmt25, 100 );
+
+        PlayerParameter pylp13 = new PlayerParameter( player27, prmt15, 70 );
+        PlayerParameter pylp14 = new PlayerParameter( player27, prmt21, 70 );
+        PlayerParameter pylp15 = new PlayerParameter( player27, prmt25, 70);
+
+        PlayerParameter pylp16 = new PlayerParameter( player28, prmt15, 20 );
+        PlayerParameter pylp17 = new PlayerParameter( player28, prmt21, 20 );
+        PlayerParameter pylp18 = new PlayerParameter( player28, prmt25, 20 );
+
+        PlayerParameter pylp19 = new PlayerParameter( player29, prmt15, 60 );
+        PlayerParameter pylp20 = new PlayerParameter( player29, prmt21, 60 );
+        PlayerParameter pylp21 = new PlayerParameter( player29, prmt25, 60 );
+
+        PlayerParameter pylp22 = new PlayerParameter( player30, prmt15, 40 );
+        PlayerParameter pylp23 = new PlayerParameter( player30, prmt21, 40 );
+        PlayerParameter pylp24 = new PlayerParameter( player30, prmt25, 40 );
+
+        PlayerParameter pylp25 = new PlayerParameter( player31, prmt15, 10 );
+        PlayerParameter pylp26 = new PlayerParameter( player31, prmt21, 10 );
+        PlayerParameter pylp27 = new PlayerParameter( player31, prmt25, 10 );
+
+        PlayerParameter pylp28 = new PlayerParameter( player32, prmt15, 80 );
+        PlayerParameter pylp29 = new PlayerParameter( player32, prmt21, 80 );
+        PlayerParameter pylp30 = new PlayerParameter( player32, prmt25, 80 );
+
+        // java.util.Random random = new java.util.Random();
+        // PlayerParameter pylp1 = new PlayerParameter( player23, prmt15, random.nextInt(1, 100) );
+        // PlayerParameter pylp2 = new PlayerParameter( player23, prmt21, random.nextInt(1, 100) );
+        // PlayerParameter pylp3 = new PlayerParameter( player23, prmt25, random.nextInt(1, 100) );
+
+        // PlayerParameter pylp4 = new PlayerParameter( player24, prmt15, random.nextInt(1, 100) );
+        // PlayerParameter pylp5 = new PlayerParameter( player24, prmt21, random.nextInt(1, 100) );
+        // PlayerParameter pylp6 = new PlayerParameter( player24, prmt25, random.nextInt(1, 100) );
+        
+        // PlayerParameter pylp7 = new PlayerParameter( player25, prmt15, random.nextInt(1, 100) );
+        // PlayerParameter pylp8 = new PlayerParameter( player25, prmt21, random.nextInt(1, 100) );
+        // PlayerParameter pylp9 = new PlayerParameter( player25, prmt25, random.nextInt(1, 100) );
+        
+        // PlayerParameter pylp10 = new PlayerParameter( player26, prmt15, random.nextInt(1, 100) );
+        // PlayerParameter pylp11 = new PlayerParameter( player26, prmt21, random.nextInt(1, 100) );
+        // PlayerParameter pylp12 = new PlayerParameter( player26, prmt25, random.nextInt(1, 100) );
+        
+        // PlayerParameter pylp13 = new PlayerParameter( player27, prmt15, random.nextInt(1, 100) );
+        // PlayerParameter pylp14 = new PlayerParameter( player27, prmt21, random.nextInt(1, 100) );
+        // PlayerParameter pylp15 = new PlayerParameter( player27, prmt25, random.nextInt(1, 100) );
+        
+        // PlayerParameter pylp16 = new PlayerParameter( player28, prmt15, random.nextInt(1, 100) );
+        // PlayerParameter pylp17 = new PlayerParameter( player28, prmt21, random.nextInt(1, 100) );
+        // PlayerParameter pylp18 = new PlayerParameter( player28, prmt25, random.nextInt(1, 100) );
+        
+        // PlayerParameter pylp19 = new PlayerParameter( player29, prmt15, random.nextInt(1, 100) );
+        // PlayerParameter pylp20 = new PlayerParameter( player29, prmt21, random.nextInt(1, 100) );
+        // PlayerParameter pylp21 = new PlayerParameter( player29, prmt25, random.nextInt(1, 100) );
+        
+        // PlayerParameter pylp22 = new PlayerParameter( player30, prmt15, random.nextInt(1, 100) );
+        // PlayerParameter pylp23 = new PlayerParameter( player30, prmt21, random.nextInt(1, 100) );
+        // PlayerParameter pylp24 = new PlayerParameter( player30, prmt25, random.nextInt(1, 100) );
+        
+        // PlayerParameter pylp25 = new PlayerParameter( player31, prmt15, random.nextInt(1, 100) );
+        // PlayerParameter pylp26 = new PlayerParameter( player31, prmt21, random.nextInt(1, 100) );
+        // PlayerParameter pylp27 = new PlayerParameter( player31, prmt25, random.nextInt(1, 100) );
+        
+        // PlayerParameter pylp28 = new PlayerParameter( player32, prmt15, random.nextInt(1, 100) );
+        // PlayerParameter pylp29 = new PlayerParameter( player32, prmt21, random.nextInt(1, 100) );
+        // PlayerParameter pylp30 = new PlayerParameter( player32, prmt25, random.nextInt(1, 100) );
+
+        playerParameterRepository.saveAll (
+            Arrays.asList (
+                pylp1, pylp2, pylp3, pylp4, pylp5, pylp6, pylp7, pylp8, pylp9, pylp10, pylp11, pylp12, pylp13, pylp14, pylp15, pylp16, pylp17, pylp18, pylp19, pylp20, pylp21, pylp22, pylp23, pylp24, pylp25, pylp26, pylp27, pylp28, pylp29, pylp30
+            )
+        );
     
     }
     
