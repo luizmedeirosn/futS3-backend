50f5ed7f5647c83242257ee6a88a926eef209f20
luizmedeirosn <netomedeirosdev@gmail.com>
2023-09-21 22:45:06 -0300

    refactor: implementing DTO pattern in Position controller

--- src/main/java/com/luizmedeirosn/futs3/controllers/PositionController.java
+++ src/main/java/com/luizmedeirosn/futs3/controllers/PositionController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.luizmedeirosn.futs3.dto.input.post.PostPositionDTO;
 import com.luizmedeirosn.futs3.dto.input.update.UpdatePositionDTO;
 import com.luizmedeirosn.futs3.dto.output.PositionDTO;
 import com.luizmedeirosn.futs3.services.PositionService;
@@ -41,8 +42,8 @@ public class PositionController {
     }
 
     @PostMapping
-    public ResponseEntity<PositionDTO> save(@RequestBody PositionDTO positionDTO) {
-        positionDTO = service.save(positionDTO);
+    public ResponseEntity<PositionDTO> save(@RequestBody PostPositionDTO postPositionDTO) {
+        PositionDTO positionDTO = service.save(postPositionDTO);
         URI uri = 
             ServletUriComponentsBuilder
             .fromCurrentRequest()
@@ -54,12 +55,11 @@ public class PositionController {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<PositionDTO> updateById(@PathVariable Long id, @RequestBody UpdatePositionDTO obj) {
-        PositionDTO entity = service.update(id, obj);
-        ResponseEntity<PositionDTO> response = ResponseEntity.ok().body(entity);
+    public ResponseEntity<PositionDTO> updateById(@PathVariable Long id, @RequestBody UpdatePositionDTO updatePositionDTO) {
+        PositionDTO positionDTO = service.update(id, updatePositionDTO);
+        ResponseEntity<PositionDTO> response = ResponseEntity.ok().body(positionDTO);
         return response;
     }
-
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
