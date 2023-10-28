2c77e42a6a22a3c4c722644a66589d43d489e955
luizmedeirosn <netomedeirosdev@gmail.com>
2023-10-28 17:56:30 -0300

    style: descriptions of the Positions

--- src/main/java/com/luizmedeirosn/futs3/configs/TestConfig.java
+++ src/main/java/com/luizmedeirosn/futs3/configs/TestConfig.java
@@ -67,21 +67,35 @@ public class TestConfig implements CommandLineRunner {
             )
         );
         
-        Position pos1 = new Position( "GOLEIRO",            "GOLEIRO: defensor do gol" );
-        Position pos2 = new Position( "LATERAL DIREITO",    "LATERAL DIREITO: responsável por avançar e apoiar a defesa" );
-        Position pos3 = new Position( "ZAGUEIRO DESTRO",    "ZAGUEIRO DESTRO: responsável pela proteção do gol" );
-        Position pos4 = new Position( "ZAGUEIRO CANHOTO",   "ZAGUEIRO CANHOTO: responsável pela proteção do gol" );
-        Position pos5 = new Position( "LATERAL ESQUERDO",   "LATERAL ESQUERDO: responsável por avançar e apoiar a defesa" );
-        Position pos6 = new Position( "TERCEIRO ZAGUEIRO",  "TERCEIRO ZAGUEIRO: zagueiro que atua como um líbero, auxiliando a defesa" );
-        Position pos7 = new Position( "PRIMEIRO VOLANTE",   "PRIMEIRO VOLANTE: responsável pela marcação e pela transição da defesa para o ataque" );
-        Position pos8 = new Position( "SEGUNDO VOLANTE",    "SEGUNDO VOLANTE: responsável por auxiliar o primeiro volante, equilibrando o meio de campo" );
-        Position pos9 = new Position( "MEIA DIREITO",       "MEIA DIREITO: responsável pela criação de jogadas no meio de campo" );
-        Position pos10 = new Position( "MEIA ESQUERDO",     "MEIA ESQUERDO: responsável pela criação de jogadas no meio de campo" );
-        Position pos11 = new Position( "MEIA-ATACANTE",     "MEIA-ATACANTE: jogador que atua tanto na criação quanto na finalização das jogadas" );
-        Position pos12 = new Position( "PONTA DIREIRA",     "PONTA DIREITA: responsável por avançar pelas laterais e cruzar bolas na área" );
-        Position pos13 = new Position( "PONTA ESQUERDA",    "PONTA ESQUERDA: responsável por avançar pelas laterais e cruzar bolas na área" );
-        Position pos14 = new Position( "ATACANTE",          "ATACANTE: responsável por concluir as jogadas, marcando gols" );
-        Position pos15 = new Position( "CENTROAVANTE",      "CENTROAVANTE: jogador que joga mais próximo à área adversária, responsável por concluir as jogadas com gols" );
+        Position pos1 = new Position( "GOLEIRO", "Defensor do gol, responsável por proteger a meta e realizar defesas fundamentais para a equipe." );
+
+        Position pos2 = new Position( "LATERAL DIREITO", "Contribui para a defesa e apoia o ataque, fornecendo assistência aos atacantes com avanços pelo lado direito do campo." );
+
+        Position pos3 = new Position( "ZAGUEIRO DESTRO", "Encarregado da marcação sólida e da proteção do gol, ocasionalmente atuando como uma surpresa tática." );
+
+        Position pos4 = new Position( "ZAGUEIRO CANHOTO", "Similar ao zagueiro destro, responsável pela defesa e também podendo surpreender estrategicamente." );
+
+        Position pos5 = new Position( "LATERAL ESQUERDO", "Desempenha papel defensivo e ofensivo, ajudando na defesa e prestando assistência aos atacantes pelo lado esquerdo do campo" );
+
+        Position pos6 = new Position( "TERCEIRO ZAGUEIRO", "Zagueiro que atua como líbero, oferecendo suporte adicional à defesa em momentos estratégicos." );
+
+        Position pos7 = new Position( "PRIMEIRO VOLANTE", "Encarregado da marcação e da transição defesa-ataque, desempenhando um papel vital na distribuição do jogo." );
+
+        Position pos8 = new Position( "SEGUNDO VOLANTE", "Auxilia o primeiro volante, equilibrando o meio de campo e contribuindo tanto na defesa quanto no ataque." );
+
+        Position pos9 = new Position( "MEIA DIREITO", "Focado na criação de jogadas no meio do campo, buscando oportunidades de ataque pelo lado direito." );
+
+        Position pos10 = new Position( "MEIA ESQUERDO", "Similar ao meia direito, concentra-se na criação de jogadas, explorando oportunidades de ataque pelo lado esquerdo." );
+
+        Position pos11 = new Position( "MEIA-ATACANTE", "Atua tanto na criação quanto na finalização das jogadas, sendo uma peça-chave na articulação do jogo." );
+
+        Position pos12 = new Position( "PONTA DIREIRA", "Avança pelas laterais direitas, cruzando bolas na área e contribuindo com o ataque do time." );
+        
+        Position pos13 = new Position( "PONTA ESQUERDA", "Similar à ponta direita, avança pelas laterais esquerdas, cruzando bolas na área e participando do jogo ofensivo." );
+
+        Position pos14 = new Position( "ATACANTE", "Encarregado de concluir jogadas, seja realizando passes decisivos ou marcando gols para a equipe." );
+
+        Position pos15 = new Position( "CENTROAVANTE", "Jogador que atua próximo à área adversária, responsável por finalizar jogadas com gols, sendo a referência principal do ataque." );
 
         positionRepository.saveAll (
             Arrays.asList (
