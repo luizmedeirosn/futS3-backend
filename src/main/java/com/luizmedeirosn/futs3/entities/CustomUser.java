a489b705f0c2956c65ad641249cd34f1d73456df
netomedeirosdev <netomedeirosdev@gmail.com>
2024-01-19 19:45:14 -0300

    Feat user classes

---
+++ src/main/java/com/luizmedeirosn/futs3/entities/CustomUser.java
@@ -0,0 +1,36 @@
+package com.luizmedeirosn.futs3.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CustomUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    @Column(nullable = false)
+    private String roles;
+
+}
---
+++ src/main/java/com/luizmedeirosn/futs3/security/jwt/CustomUserDetails.java
@@ -0,0 +1,75 @@
+package com.luizmedeirosn.futs3.security.jwt;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.luizmedeirosn.futs3.entities.CustomUser;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CustomUserDetails implements UserDetails {
+
+    private String username;
+    private String password;
+    private List<GrantedAuthority> authorities;
+
+    public CustomUserDetails(CustomUser customUser) {
+        username = customUser.getUsername();
+        password = customUser.getPassword();
+        authorities = Arrays
+                .stream(customUser.getRoles().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
