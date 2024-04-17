28a4cc9b102fc5a24f7a48dd707036493b62922d
luizmedeirosn <netomedeirosdev@gmail.com>
2024-04-17 00:23:40 -0300

    Perf use Argon 2 for password encoder

--- src/main/resources/db/migration/V17__Insert_Data_In_Tb_User.sql
+++ src/main/resources/db/migration/V17__Insert_Data_In_Tb_User.sql
@@ -1,5 +1,5 @@
 INSERT INTO tb_user
     (username, email, password, roles)
 VALUES
-    ('admin', 'admin@futS3.com', '$2a$10$uBGLh4UgREdn8NAlJ9U2XuO6KF0giqsKMIidfjGvj8XkiTvwvs5Rq', 'ROLE_ADMIN'), -- PASSWORD: '!@Futs3$$$19V4A'
-    ('user', 'user@futS3.com', '$2a$10$uBGLh4UgREdn8NAlJ9U2XuO6KF0giqsKMIidfjGvj8XkiTvwvs5Rq', 'ROLE_USER'); -- PASSWORD: '!@Futs3$$$19V4A'
+    ('admin', 'admin@futS3.com', '$argon2id$v=19$m=16384,t=2,p=1$ZQ8UUj94/Z65EP0DTaJ/MA$gda5jaXHRDtSnUlNRBbmbC9HEpAJVPtJhb5aHLxAcTI', 'ROLE_ADMIN'), -- PASSWORD: '!@Futs3$$$19V4A'
+    ('user', 'user@futS3.com', '$argon2id$v=19$m=16384,t=2,p=1$ZQ8UUj94/Z65EP0DTaJ/MA$gda5jaXHRDtSnUlNRBbmbC9HEpAJVPtJhb5aHLxAcTI', 'ROLE_USER'); -- PASSWORD: '!@Futs3$$$19V4A'
