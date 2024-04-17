a834b1991b7977d337e4fe99108acc6d5f7e4076
luizmedeirosn <netomedeirosdev@gmail.com>
2024-04-17 13:25:45 -0300

    Refactor sequence of migrations

--- src/main/java/com/luizmedeirosn/futs3/Futs3Application.java
+++ src/main/java/com/luizmedeirosn/futs3/Futs3Application.java
@@ -2,13 +2,11 @@ package com.luizmedeirosn.futs3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @SpringBootApplication
 public class Futs3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Futs3Application.class, args);
-		System.out.println(Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8().encode("!@Futs3$$$19V4A"));
 	}
 }
