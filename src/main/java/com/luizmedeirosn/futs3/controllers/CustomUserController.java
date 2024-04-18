9ff94783ed3ca5a5eeefa58b3490367db754c6b1
luizmedeirosn <netomedeirosdev@gmail.com>
2024-04-18 00:47:29 -0300

    Feat authorization for user findAll and findById endpoints

--- src/main/java/com/luizmedeirosn/futs3/controllers/CustomUserController.java
+++ src/main/java/com/luizmedeirosn/futs3/controllers/CustomUserController.java
@@ -4,6 +4,7 @@ import com.luizmedeirosn.futs3.entities.CustomUser;
 import com.luizmedeirosn.futs3.services.CustomUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,11 +23,13 @@ public class CustomUserController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<List<CustomUser>> findAll() {
         return ResponseEntity.ok().body(customUserService.findAll());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<CustomUser> findById(@PathVariable @NonNull Long id) {
         return ResponseEntity.ok().body(customUserService.findById(id));
     }
